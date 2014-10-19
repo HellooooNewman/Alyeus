d536a64ca542906dbcbdfee09cea958c78a152ba
Justplaincancer <knewmanium@gmail.com>
2014-10-19 15:49:36 -0400

    Validation and Video Formats
    Fixed Validation and CSS errors. Change some typography. Added more
    support for video formats

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ window.onload=function init() {
 function map(){ 
   source = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2988634.1870639385!2d-81.243177!3d42.9869502!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ef20ea88d9b0b%3A0x28c7d7699a056b95!2sLondon%2C+ON!5e0!3m2!1sen!2sca!4v1412898361667";
   document.getElementById("map").style.display="none";
-  iframe = document.getElementById("iframe");
-  iframe.src = source;  
-  iframe.height = "450px";
+  Map = document.getElementById("mapIframe");
+  Map.src = source;  
+  Map.height = "450px";
   
 }
 
@@ -25,7 +25,7 @@ function expand(){
   var all = document.getElementsByClassName('bandInfo');
 
     for (var i = 0; i < all.length; i++) {
-    all[i].style.height = '260px';
+    all[i].style.height = '270px';
 
     } 
     expanded = true;
